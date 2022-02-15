38603698943a1733069b8f0eb0ea3386c0f1c1d6
saimeerasingh <singhsaimeera@gmail.com>
2022-02-15 14:00:26 +0000

    vehicle class setup

--- src/main/java/Vehicle.java
+++ src/main/java/Vehicle.java
@@ -1,2 +1,30 @@
-public class Vehicle {
+public abstract class Vehicle {
+
+    private int price;
+    private String color;
+    private String make;
+    private String model;
+
+    public Vehicle(int price, String color, String make, String model) {
+        this.price = price;
+        this.color = color;
+        this.make = make;
+        this.model = model;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
 }
